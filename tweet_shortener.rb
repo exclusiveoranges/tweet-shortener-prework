bc65118b21f34fa66914e8a31c9d9310dbe248c7
Ryan O'Toole <otoolerj@gmail.com>
2018-03-26 17:30:45 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,43 @@
-# Write your code here.
+require 'pry'
+def dictionary
+  dictionary =
+  {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@" ,
+    "and" => "&"
+  }
+end
+
+
+
+def word_substituter(tweet_string)
+  tweet_array = tweet_string.split
+  tweet_array.collect do |string|
+    if dictionary.keys.include?(string.downcase)
+        dictionary[string.downcase]
+    else
+        string
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweet_array)
+  y = tweet_array.join
+  z = y.split
+  x = z.collect do |string|
+    if dictionary.include?(string.downcase)
+        dictionary[string.downcase]
+    else
+        string
+    end
+  end
+  binding.pry
+  puts x.join(" ")
+end
